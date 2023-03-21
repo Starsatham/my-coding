ee512353afa9bea0449fc45f745b8adddc4385ec
riyaz <riyazahamedsana@gmail.com>
2023-03-21 15:17:40 +0530

    riyaz edited

--- hello.java
+++ hello.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("it is me"); 
+// riyaz edit
+System.out.println("it is you");
     }
 }
